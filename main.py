7dd1b4337d1a3e624ee0bdece9b2d1024a2b08d6
Sumegh20 <sumegh20@gmail.com>
2022-11-12 00:18:11 +0530

    Upload all files

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import sys, os
+from sensor.logger import logging
+from sensor.pipeline import traning_pipeline
+from sensor.pipeline.traning_pipeline import TrainPipeline
 
+#from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 
-if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print("collection name:",mongodb_client.database.list_collection_names())
+if __name__ == "__main__":
+    try:
+        traning_pipeline = TrainPipeline()
+        traning_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
